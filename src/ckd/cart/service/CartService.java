8006eb955a960d97bd15139877d336cf6eecd942
Lucy <khmin09015@gmail.com>
2021-05-04 22:44:44 +0900

    Add /view/cart/cartListInfo.jsp Add servlet file /cart/cartListInfo.jsp Add vo file /vo/Cart.java Add service file /service/CartService.java

---
+++ src/ckd/cart/service/CartService.java
@@ -0,0 +1,28 @@
+package ckd.cart.service;
+
+import java.util.List;
+
+import ckd.cart.vo.Cart;
+
+public class CartService {
+	// 장바구니 목록 조회
+	public List<Cart> InquiryCartList() {
+		List<Cart> list  = null;
+		
+		return list;
+	}
+	
+	// 장바구니 등록
+	public int registerCartInfo(Cart cart, String email) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 장바구니 삭제
+	public int removeCartInfo(String cartNo) {
+		int result = 0;
+		
+		return result;
+	}
+}
